94a9213b5f254d07a251d9d522240dd1e808baf6
corentin <corentinalcoy@gmail.com>
2020-02-05 06:55:26 +0100

    remove unsued code

--- src/socialite/actions.py
+++ src/socialite/actions.py
@@ -1,28 +1,3 @@
-from social_core.utils import sanitize_redirect
-
-
-def do_auth(backend, redirect_name='next'):
-    data = backend.strategy.request_data(merge=True)
-
-    # Save extra data into session.
-    for field_name in backend.setting('FIELDS_STORED_IN_SESSION', []):
-        if field_name in data:
-            backend.strategy.session_set(field_name, data[field_name])
-
-    if redirect_name in data:
-        # Check and sanitize a user-defined GET/POST next field value
-        redirect_uri = data[redirect_name]
-        if backend.setting('SANITIZE_REDIRECTS', True):
-            allowed_hosts = backend.setting('ALLOWED_REDIRECT_HOSTS', []) + \
-                            [backend.strategy.request_host()]
-            redirect_uri = sanitize_redirect(allowed_hosts, redirect_uri)
-        backend.strategy.session_set(
-            redirect_name,
-            redirect_uri or backend.setting('LOGIN_REDIRECT_URL')
-        )
-    return backend.start()
-
-
 def do_complete(backend, user=None, redirect_name='next', *args, **kwargs):
     data = backend.strategy.request_data()
 

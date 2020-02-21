e0ee0a4a2912b315bd81e36ab63498d2c27aabb6
corentin <corentin@trybeans.com>
2020-02-21 11:04:12 -0500

    - fix bug - fix travis config

--- src/socialite/oauth/github.py
+++ src/socialite/oauth/github.py
@@ -7,4 +7,4 @@ class GithubAPI(object):
     def __init__(self, token, **kwargs):
         client_id = get_config('socialite.SOCIAL_AUTH_GITHUB_KEY')
         self.oauth_session = BaseOAuth2(client_id, token=token)
-        self.oauth_session.BASE_URL = f'https://api.github.com'
+        self.oauth_session.BASE_URL = 'https://api.github.com'
